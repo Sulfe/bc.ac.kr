b87b7497b81a207354bfe9c90e8c2dcc44b0b397
Sulfe <88019008+Sulfe@users.noreply.github.com>
2022-06-21 17:05:51 +0900

    2022-06-21

---
+++ java/GlobalObjectProg/src/jp/co/bbreak/sokusen/_1/_2/Test01.java
@@ -0,0 +1,10 @@
+package jp.co.bbreak.sokusen._1._2;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
