7284537fc629de734a3e031f0d91eab83cc513ca
Sulfe <88019008+Sulfe@users.noreply.github.com>
2022-07-27 14:37:59 +0900

    2022-07-27

--- java/ExcelPoi/src/bu/ac/kr/LoadExcel.java
+++ java/ExcelPoi/src/bu/ac/kr/LoadExcel.java
@@ -1,13 +1,85 @@
 package bu.ac.kr;
 
-import java.awt.Desktop;
-import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class LoadExcel {
 
 	public static void main(String args[]) {
-	
 
+		List<UserVo> list = new ArrayList<UserVo>();
+
+		try {
+			FileInputStream file = new FileInputStream("src/bu/resorces/test.xlsx");
+			@SuppressWarnings("resource")
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			int rowindex = 0;
+			int columnindex = 0;
+			XSSFSheet sheet = workbook.getSheetAt(0); // 시트 수
+			int rows = sheet.getPhysicalNumberOfRows(); // 행 수
+
+			for (rowindex = 1; rowindex < rows; rowindex++) {
+				XSSFRow row = sheet.getRow(rowindex);
+				UserVo uv = new UserVo();
+				if (row != null) {
+					int cells = row.getPhysicalNumberOfCells(); // 셀 수
+					for (columnindex = 0; columnindex <= cells; columnindex++) {
+						XSSFCell cell = row.getCell(columnindex);
+						String value = "";
+
+						if (cell == null) {
+							continue;
+						} else {
+							switch (cell.getCellType()) {
+							case XSSFCell.CELL_TYPE_FORMULA:
+								value = cell.getCellFormula();
+								break;
+							case XSSFCell.CELL_TYPE_STRING:
+								value = cell.getStringCellValue();
+								break;
+							case XSSFCell.CELL_TYPE_NUMERIC:
+								value = cell.getNumericCellValue() + "";
+								break;
+							case XSSFCell.CELL_TYPE_BOOLEAN:
+								value = cell.getBooleanCellValue() + "";
+								break;
+							case XSSFCell.CELL_TYPE_ERROR:
+								value = cell.getErrorCellValue() + "";
+								break;
+							default:
+								break;
+							}
+							switch (columnindex) {
+							case 0:
+								uv.setUserName(value);
+								break;
+							case 1:
+								uv.setUserAge(value);
+								break;
+							case 2:
+								uv.setUserSex(value);
+								break;
+							default:
+								break;
+							}
+
+						}
+
+					} // for columnindex
+				}
+				list.add(uv);
+			} // for rowIndex
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(list);
 	}
 
 }
--- java/GlobalObjectProg/src/bu4/ac/kr/BankTransactionAnalyzerSimple.java
+++ java/GlobalObjectProg/src/bu4/ac/kr/BankTransactionAnalyzerSimple.java
@@ -10,31 +10,26 @@ import java.util.Scanner;
 
 public class BankTransactionAnalyzerSimple {
 
-	private static final String RESOURCES = "src/bu4/resources/"; //파일 경로
-	private static final String file_comma = "bank,data,simple.csv"; //콤마csv파일이름
-	private static final String file_hyphen = "bank-data-simple.csv"; //하이픈 csv파일이름
+	private static final String RESOURCES = "src/bu4/resources/"; // 파일 경로
+	private static final String file_comma = "bank,data,simple.csv"; // 콤마csv파일이름
+	private static final String file_hyphen = "bank-data-simple.csv"; // 하이픈 csv파일이름
 
 	public static void main(final String... args) throws IOException {
 		// TODO Auto-generated method stub
 
-		ParseCSV pc = new ParseCSV(); // 구분자를 통해 csv을 split하는 객체
-		final Path path_comma = Paths.get(RESOURCES + file_comma); // 콤마로 구분된 csv의 경로를 가진 객체
-		final Path path_hyphen = Paths.get(RESOURCES + file_hyphen); // 하이픈으로 구분된 csv의 경로를 지닌 객체
-		final List<String> lines_comma = Files.readAllLines(path_comma); // 콤마로 구분된 csv의 내용을 가져와 list에 저장
-		final List<String> lines_hyphen = Files.readAllLines(path_hyphen); // 하이픈으로 구분된 csv의 내용을 가져와 list에 저장
+		ParseCSV pc = new ParseCSV();
+		final Path path_comma = Paths.get(RESOURCES + file_comma);
+		final Path path_hyphen = Paths.get(RESOURCES + file_hyphen);
+		final List<String> lines_comma = Files.readAllLines(path_comma);
+		final List<String> lines_hyphen = Files.readAllLines(path_hyphen);
 
-		List<BankTransaction> comma = pc.commaLinesFromCSV(lines_comma); // 리스트 콤마에 pc 객체의 구분자 콤마로 구분하는 메소드를 이용해 결과를 받아
-																			// 리스트에 저장
-		List<BankTransaction> hyphen = pc.hyphenLinesFromCSV(lines_hyphen); // 리스트 하이픈에 pc객체의 구분자 하이픈으로 구분하는 메소드를 이용해
-																			// 결과를 받아 리스트에 저장
-
-		System.out.println(comma);
-		System.out.println(hyphen);
+		System.out.println(pc.lineFromCSV(lines_comma, ","));
+		System.out.println(pc.lineFromCSV(lines_hyphen, "-"));
 
 		// list for files name
 		CheckResourcesFiles chf = new CheckResourcesFiles(RESOURCES); // 파일 경로를 입력받아 해당 경로의 파일관련 처리를 담당하는 객체 chf를 생성
 		File fileList[] = chf.fileList(); // 파일의 목록을 얻기위해 배열 fileList 에 해당 경로의 파일 목록을 리스트로 리턴하는 함수를 사용하여 값을 입력
-		System.out.println("\n"+RESOURCES+"경로의 파일 목록");
+		System.out.println("\n" + RESOURCES + "경로의 파일 목록");
 		for (File i : fileList) { // for문 반복을 통해 파일 리스트 출력
 			System.out.println(i);
 		}
@@ -46,9 +41,9 @@ public class BankTransactionAnalyzerSimple {
 		sc.close(); // 사용한 스캐너 종료
 		System.out.println(chf.verify(fileName)); // chf 객체의 파일 명이 있는지 확인하는 verify 메소드를 사용하여 boolean값을 출력
 
-		
-		Runfile rf = new Runfile();
-		rf.run(RESOURCES+fileName);
+		// Run Program
+		Runfile rf = new Runfile(); // 파일 실행 기능을 담당하는 런파일 객체 생성
+		rf.run(RESOURCES + fileName); // 파일 경로와 파일 명을 입력받아 해당 경로 프로그램 실행
 	}
 
 }
